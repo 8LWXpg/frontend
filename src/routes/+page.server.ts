97d38a92eaffdc5c95069b250ade5b4e30c1cedf
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2023-12-18 21:39:27 +0800

    button

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,4 @@
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
-    logout: async ({ cookies }) => {
-        cookies.delete("session");
-        throw redirect(303, "/");
-    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
 	let showModal = false;
+	let cpu = 0;
+	let mem = 0;
+	let bw = 0;
+	let dly = 0;
+
+	let tempCpu = 0;
+	let tempMem = 0;
+	let tempBw = 0;
+	let tempDly = 0;
 
 	function toggleModal() {
 		showModal = !showModal;
+	}
+
+	function saveSettings() {
+		cpu = tempCpu;
+		mem = tempMem;
+		bw = tempBw;
+		dly = tempDly;
+		toggleModal();
 	}
 </script>
 
@@ -17,11 +34,36 @@
 	{#if showModal}
 		<div class="modal">
 			<div class="center settings">
-				<!-- Settings page content goes here -->
-				<div class="settings-footer">
-					<!-- <button on:click={saveSettings}>Save</button> -->
-					<button on:click={toggleModal}>Close</button>
-				</div>
+				<form>
+					<label for="cpu">CPU Utilization Max</label>
+					<input
+						type="number"
+						min="0"
+						max="100"
+						id="cpu"
+						bind:value={tempCpu}
+					/>
+					<label for="mem">Memory Utilization Max</label>
+					<input
+						type="number"
+						min="0"
+						max="100"
+						id="mem"
+						bind:value={tempMem}
+					/>
+					<label for="bw">Bandwidth Utilization Max</label>
+					<input
+						type="number"
+						min="0"
+						max="100"
+						id="bw"
+						bind:value={tempBw}
+					/>
+					<label for="dly">ICMP Delay</label>
+					<input type="number" id="dly" bind:value={tempDly} />
+					<button on:click={toggleModal}>Cancel</button>
+					<button on:click={saveSettings}>Save</button>
+				</form>
 			</div>
 		</div>
 	{/if}
