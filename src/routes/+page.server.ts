b2e3c6ab50aab629bd10fc6dbb63c98873866909
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2023-12-18 21:57:13 +0800

    get token

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,35 @@
-import { redirect } from '@sveltejs/kit';
+import type { P } from "vitest/dist/reporters-5f784f42.js";
 
-export const actions = {
+const url = 'http://localhost/zabbix/api_jsonrpc.php';
+const headers = {
+    'Content-Type': 'application/json-rpc'
+};
+
+async function getToken(): Promise<string> {
+    const data = {
+        jsonrpc: '2.0',
+        method: 'user.login',
+        params: {
+            username: 'Admin',
+            password: 'zabbix'
+        },
+        id: 1
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+        }).then(res => res.json()).then(result => {
+            if (result.result) {
+                resolve(result.result);
+            } else {
+                reject('No result found');
+            }
+        }).catch(err => {
+            console.error('Error:', err);
+            reject(err);
+        });
+    });
 }
