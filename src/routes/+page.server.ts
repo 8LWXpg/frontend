5b11f5e4ebbe30719bd9a2d1180cbd57cafb6e38
Obingping <ss182114@gmail.com>
2023-12-18 21:49:26 +0800

    [Update]

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,3 +2,27 @@ import { redirect } from '@sveltejs/kit';
 
 export const actions = {
 }
+
+import axios from 'axios';
+
+export const actions = {
+    async login(request) {
+        const response = await axios.post('http://localhost/zabbix/api_jsonrpc.php', {
+            jsonrpc: '2.0',
+            method: 'user.login',
+            params: {
+                username: 'Admin',
+                password: 'zabbix'
+            },
+            id: 1
+        }, {
+            headers: {
+                'Content-Type': 'application/json-rpc'
+            }
+        });
+
+        return {
+            body: response.data
+        };
+    }
+}
